abb08ef9ad0cd55b4a22e560ab49e64bc9d6af4c
abhi <abhionly009@gmail.com>
2021-09-30 22:37:24 +0530

    Added dependency for redux and redux toolkit and created UI for updating product.

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductCard.css";
 import { DeleteRounded } from "@material-ui/icons";
 import { EditRounded } from "@material-ui/icons";
 
 function ProductCard({ name, price, imageUrl, brand }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const deleteHandler = () => {
     alert("Are sure you want to delete this product");
   };
 
-  const updateHandler = () => {};
+  const updateHandler = () => {
+    setIsModalOpen(true);
+    console.log(brand, name, price);
+  };
+
+  const closeModalHandler = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="product__card">
+      <div className={isModalOpen ? "show__modal" : "hide__modal"}>
+        <input type="text" name="" id="" value={brand} />
+        <input type="text" name="" id="" value={name} />
+        <input type="text" name="" id="" value={price} />
+        <input type="text" name="" id="" value={imageUrl} />
+
+        <button className="updateBtn">Update</button>
+        <button className="cancelBtn" onClick={closeModalHandler}>
+          Cancel
+        </button>
+      </div>
+
       <p className="brand">{brand}</p>
       <p className="name">{name}</p>
       <img src={imageUrl} alt={name} />
